cd31614c051c0a23ecb173f65615fab58f0e20d3
qaywsx22 <moyseevich@web.de>
2025-10-15 21:19:49 +0200

    add form submit functionality

--- index.js
+++ index.js
@@ -127,3 +127,25 @@ stars.forEach((star, i) => {
 })
     
 starContainer?.addEventListener('mouseleave', hideActiveStar)
+
+async function sendData(formElement) {
+  const action = "https://api.staticforms.xyz/submit"
+  const formData = new FormData(formElement);
+  try {
+    const response = await fetch(action, {
+      method: "POST",
+      // Set the FormData instance as the request body
+      body: formData,
+    });
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+document.querySelectorAll("form").forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendData(event.target);
+  });
+});
